c8fb9c3792a36956021fa391dd392cab52c8a3b9
Lucas <lucaspoliveira822@gmail.com>
2025-11-24 11:01:07 -0300

    first commit

---
+++ rest-with-spring-boot-and-java-study/src/main/java/com/educational/app/controllers/GreetingController.java
@@ -0,0 +1,21 @@
+package com.educational.app.controllers;
+
+import com.educational.app.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name",defaultValue = "World!") String name){
+        return new Greeting (counter.incrementAndGet(),String.format(template,name));
+    }
+
+}
---
+++ rest-with-spring-boot-and-java-study/src/main/java/com/educational/app/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.educational.app.model;
+
+public record Greeting(Long id, String content) {
+}
